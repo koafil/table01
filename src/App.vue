a85de2d37b8f743954081de3a6420d82f3773537
kfil <koafil@gmail.com>
2023-10-08 20:46:08 +0300

    my data

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <el-table :data="data" style="width: 100%">
-  <el-table-column prop="id" label="#" width="70" sortable/>
-  <el-table-column prop="title" label="Название" width="180" />
-  <el-table-column prop="body" label="Текст" />
+    <el-table-column prop="id" label="#" width="55" sortable/>
+    <el-table-column prop="used" label="Used" width="55" />
+    <el-table-column prop="articul" label="Артикул" width="155" />
+  <el-table-column prop="date" label="Дата" width="190" />
+  <el-table-column prop="info" label="Текст" />
   </el-table>
   <el-pagination background layout="prev, pager, next"
                  :pager-count="4"
@@ -16,10 +18,10 @@
 import {computed, ref} from "vue";
 import { useFetch} from "@vueuse/core";
 
-const limit = ref(5)
+const limit = ref(15)
 const page  = ref(1)
 const url = computed(()=>{
-  return `https://jsonplaceholder.typicode.com/posts?_page=${page.value}&_limit=${limit.value}`
+  return `http://192.168.50.5:3002/tovars?page=${page.value-1}&npp=${limit.value}`
 })
 const {isFetching, error, data} = useFetch(url, {
   refetch: true
