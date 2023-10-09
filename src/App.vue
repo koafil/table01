f90f45a76f4b3ee1b9fefcc6de0f89edb0141dca
kfil <koafil@gmail.com>
2023-10-09 15:16:05 +0300

    Принимает от сервера общее кол-во строк total

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-table :data="data" style="width: 100%">
+  <el-table :data="data?.rowData" style="width: 100%">
     <el-table-column prop="id" label="#" width="55" sortable/>
     <el-table-column prop="used" label="Used" width="55" />
     <el-table-column prop="articul" label="Артикул" width="155" />
@@ -7,10 +7,10 @@
   <el-table-column prop="info" label="Текст" />
   </el-table>
   <el-pagination background layout="prev, pager, next"
-                 :pager-count="4"
+                 :pager-count="7"
                  :page-size="limit"
                  v-model:current-page="page"
-                 :total="100" />
+                 :total="data?.total?data.total:0" />
 </template>
 
 <script setup>
@@ -23,7 +23,7 @@ const page  = ref(1)
 const url = computed(()=>{
   return `http://192.168.50.5:3002/tovars?page=${page.value-1}&npp=${limit.value}`
 })
-const {isFetching, error, data} = useFetch(url, {
+const {isFetching, error, data } = useFetch(url, {
   refetch: true
 }).json();
 </script>
